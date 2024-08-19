0020671947b2562c5a1e7d9ac31f1a856620254d
robert <robersargsyan2023@gmail.com>
2024-08-19 17:41:32 +0300

    s

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,53 +1,51 @@
 <template>
-  <div>
-    POSTS
-  </div>
-<!--  <div class="posts-view">-->
-<!--    <div class="posts-view__inner">-->
-<!--      <div class="posts-view__filters">-->
-<!--        <el-input-->
-<!--          class="posts-view__search-field"-->
-<!--          v-model="postsStore.searchModel"-->
-<!--          style="width: 240px"-->
-<!--          placeholder="Введите текст для поиска..."-->
-<!--        />-->
-<!--        <el-select-->
-<!--          class="posts-view__select-field"-->
-<!--          v-model="postsStore.sortFieldName"-->
-<!--          placeholder="Select"-->
-<!--          size="large"-->
-<!--          style="width: 240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="item in options"-->
-<!--            :key="item.value"-->
-<!--            :label="item.label"-->
-<!--            :value="item.value"-->
-<!--          />-->
-<!--        </el-select>-->
 
-<!--        <el-button class="posts-view__button" @click="postsStore.changeDialogVisible" type="success"-->
-<!--          >Создать пост-->
-<!--        </el-button>-->
-<!--      </div>-->
-<!--      <post-dialog-->
-<!--        @create-post="postsStore.createPost"-->
-<!--        @update-post="postsStore.updatePost"-->
-<!--        :dialog-type="postsStore.dialogType"-->
-<!--      />-->
-<!--      <context-menu v-if="postsStore.showContextMenu" />-->
-<!--      <posts-list :list="postsStore.getSlicedPosts" />-->
-<!--      <el-pagination-->
-<!--        size="small"-->
-<!--        background-->
-<!--        v-model:page-size="postsStore.pageSize"-->
-<!--        v-model:current-page="postsStore.pageNumber"-->
-<!--        layout="prev, pager, next"-->
-<!--        :total="postsStore.getFilteredPosts.length"-->
-<!--        class="mt-4"-->
-<!--      />-->
-<!--    </div>-->
-<!--  </div>-->
+  <div class="posts-view">
+    <div class="posts-view__inner">
+      <div class="posts-view__filters">
+        <el-input
+          class="posts-view__search-field"
+          v-model="postsStore.searchModel"
+          style="width: 240px"
+          placeholder="Введите текст для поиска..."
+        />
+        <el-select
+          class="posts-view__select-field"
+          v-model="postsStore.sortFieldName"
+          placeholder="Select"
+          size="large"
+          style="width: 240px"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+
+        <el-button class="posts-view__button" @click="postsStore.changeDialogVisible" type="success"
+          >Создать пост
+        </el-button>
+      </div>
+      <post-dialog
+        @create-post="postsStore.createPost"
+        @update-post="postsStore.updatePost"
+        :dialog-type="postsStore.dialogType"
+      />
+      <context-menu v-if="postsStore.showContextMenu" />
+      <posts-list :list="postsStore.getSlicedPosts" />
+      <el-pagination
+        size="small"
+        background
+        v-model:page-size="postsStore.pageSize"
+        v-model:current-page="postsStore.pageNumber"
+        layout="prev, pager, next"
+        :total="postsStore.getFilteredPosts.length"
+        class="mt-4"
+      />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { usePostsStore } from '@/stores/postsStore'
